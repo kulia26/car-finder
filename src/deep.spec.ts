b92cec02d2d8918f73659ea229a9d08f59d41151
Bohdan Kulinich <kulia@macpaw.com>
2023-09-20 09:31:56 +0300

    fix: handle errors

--- src/deep.spec.ts
+++ src/deep.spec.ts
@@ -1,35 +1,45 @@
-import { test } from '@playwright/test';
-import axios from 'axios';
-import { Telegraf } from 'telegraf';
-import crypto from 'crypto';
-require('dotenv').config();
+import { test } from "@playwright/test";
+import axios from "axios";
+import { Telegraf } from "telegraf";
+import crypto from "crypto";
+require("dotenv").config();
 
 const getCarsListPageUrl = (page = 0) => {
- return `https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&categories.main.id=1&country.import.usa.not=-1&region.id[0]=10&price.USD.lte=5000&price.currency=1&abroad.not=0&custom.not=1&page=${page}&size=100`
-}
+  return `https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&categories.main.id=1&country.import.usa.not=-1&region.id[0]=10&price.USD.lte=5000&price.currency=1&abroad.not=0&custom.not=1&page=${page}&size=100`;
+};
 
-test('find cars', async ({ page }) => {
+test("find cars", async ({ page }) => {
   const promises: Array<Promise<any>> = [];
 
-  await page.goto(getCarsListPageUrl(), { waitUntil: 'networkidle' });
-  const resultsCount = parseInt((await page.locator('#staticResultsCount').allInnerTexts()).toString().replace(/ /g,''));
-  const pagesCount = Math.floor(resultsCount / 100); 
+  await page.goto(getCarsListPageUrl(), { waitUntil: "networkidle" });
+  const resultsCount = parseInt(
+    (await page.locator("#staticResultsCount").allInnerTexts())
+      .toString()
+      .replace(/ /g, ""),
+  );
+  const pagesCount = Math.floor(resultsCount / 100);
 
   for (let i = 0; i <= pagesCount; i++) {
+    await page.goto(getCarsListPageUrl(i), { waitUntil: "networkidle" });
+    const cars = await page.locator(".ticket-item").all();
 
-    try {
-      await page.goto(getCarsListPageUrl(i), { waitUntil: 'networkidle' });
-      const cars = await page.locator('.ticket-item').all();
+    console.log(`page: ${i} of ${pagesCount} ...`);
 
-      console.log(`page: ${i} of ${pagesCount} ...`);
-
-      await Promise.all(cars.map(async carElement => {
-        try {
-          const title = (await carElement.locator('.ticket-title').allInnerTexts()).toString();
-        const price = parseInt((await carElement.locator('[data-currency="USD"]').allInnerTexts()).toString().replace(/ /g,''));
-        const url = (await carElement.locator('.m-link-ticket').getAttribute('href')) || 'undefined';
-        const id =  (await carElement.getAttribute('data-advertisement-id')); 
-        const userId =  (await carElement.getAttribute('data-user-id'));
+    await Promise.all(
+      cars.map(async (carElement) => {
+        const title = (
+          await carElement.locator(".ticket-title").allInnerTexts()
+        ).toString();
+        const price = parseInt(
+          (await carElement.locator('[data-currency="USD"]').allInnerTexts())
+            .toString()
+            .replace(/ /g, ""),
+        );
+        const url =
+          (await carElement.locator(".m-link-ticket").getAttribute("href")) ||
+          "undefined";
+        const id = await carElement.getAttribute("data-advertisement-id");
+        const userId = await carElement.getAttribute("data-user-id");
         const hash = `${id}u${userId}`;
         const car = {
           title,
@@ -38,32 +48,26 @@ test('find cars', async ({ page }) => {
           hash,
         };
 
-        promises.push(axios({
-          method: 'post',
-          url: `${process.env.WEBHOOK_URL}`,
-          data: car,
-        }));
-        
-        }catch(e){
-          console.log(e);
-        }finally {
-          return Promise.resolve();
-        }
-      }));
-    
-    }catch (e) {
-      console.log(e)
-    }
-    
+        promises.push(
+          axios({
+            method: "post",
+            url: `${process.env.WEBHOOK_URL}`,
+            data: car,
+          }),
+        );
+
+        return Promise.resolve();
+      }),
+    );
   }
 
   console.log(`send to server start ` + new Date().toTimeString());
 
-  await Promise.all(promises);
+  await Promise.allSettled(promises);
 
   console.log(`send to server finish ` + new Date().toTimeString());
 
   await page.close();
-  
-  console.log({resultsCount});
-}); 
+
+  console.log({ resultsCount });
+});
