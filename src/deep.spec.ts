523d4c0afdec40cda0a1137121d81ad6b22987a4
Bohdan Kulinich <kulia@macpaw.com>
2023-10-12 11:10:34 +0300

    fix: linter

--- src/deep.spec.ts
+++ src/deep.spec.ts
@@ -1,45 +1,46 @@
-import { test } from "@playwright/test";
-import axios from "axios";
-import { Telegraf } from "telegraf";
-import crypto from "crypto";
-require("dotenv").config();
+import { test } from '@playwright/test';
+import axios from 'axios';
+import { Telegraf } from 'telegraf';
+import crypto from 'crypto';
+import { writeFile } from 'fs/promises';
+require('dotenv').config();
 
 const getCarsListPageUrl = (page = 0) => {
   return `https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&categories.main.id=1&country.import.usa.not=-1&region.id[0]=10&price.USD.lte=5000&price.currency=1&abroad.not=0&custom.not=1&page=${page}&size=100`;
 };
 
-test("find cars", async ({ page }) => {
+test('find cars', async ({ page }) => {
   const promises: Array<Promise<any>> = [];
 
-  await page.goto(getCarsListPageUrl(), { waitUntil: "networkidle" });
+  await page.goto(getCarsListPageUrl(), { waitUntil: 'networkidle' });
   const resultsCount = parseInt(
-    (await page.locator("#staticResultsCount").allInnerTexts())
+    (await page.locator('#staticResultsCount').allInnerTexts())
       .toString()
-      .replace(/ /g, ""),
+      .replace(/ /g, ''),
   );
   const pagesCount = Math.floor(resultsCount / 100);
 
   for (let i = 0; i <= pagesCount; i++) {
-    await page.goto(getCarsListPageUrl(i), { waitUntil: "networkidle" });
-    const cars = await page.locator(".ticket-item").all();
+    await page.goto(getCarsListPageUrl(i), { waitUntil: 'networkidle' });
+    const cars = await page.locator('.ticket-item').all();
 
     console.log(`page: ${i} of ${pagesCount} ...`);
 
     await Promise.all(
       cars.map(async (carElement) => {
         const title = (
-          await carElement.locator(".ticket-title").allInnerTexts()
+          await carElement.locator('.ticket-title').allInnerTexts()
         ).toString();
         const price = parseInt(
           (await carElement.locator('[data-currency="USD"]').allInnerTexts())
             .toString()
-            .replace(/ /g, ""),
+            .replace(/ /g, ''),
         );
         const url =
-          (await carElement.locator(".m-link-ticket").getAttribute("href")) ||
-          "undefined";
-        const id = await carElement.getAttribute("data-advertisement-id");
-        const userId = await carElement.getAttribute("data-user-id");
+          (await carElement.locator('.m-link-ticket').getAttribute('href')) ||
+          'undefined';
+        const id = await carElement.getAttribute('data-advertisement-id');
+        const userId = await carElement.getAttribute('data-user-id');
         const hash = `${id}u${userId}`;
         const car = {
           title,
@@ -61,11 +62,11 @@ test("find cars", async ({ page }) => {
     );
   }
 
-  console.time("sendToServer");
+  console.time('sendToServer');
 
   await Promise.allSettled(promises);
 
-  console.timeEnd("sendToServer");
+  console.timeEnd('sendToServer');
 
   await page.close();
 
